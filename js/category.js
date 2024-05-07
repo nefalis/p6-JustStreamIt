0197a2880a5cb64d18c508856d72471d08166c2b
nefalis <nefalis@gmail.com>
2024-05-07 14:23:18 +0200

    catégorie récupéré

--- js/category.js
+++ js/category.js
@@ -1,57 +1,98 @@
+displayBestMovie();
+initCategory();
+
+function initCategory(){
+    fetchDataCategory("Animation");
+    fetchDataCategory("Sci-Fi");
+    fetchDataCategory("Action");   
+}
 
 /***** Information meilleur film *****/
-fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=10&page=1')
-    .then(res => res.json())
-    .then(data => {
-        const movie = data.results[0];
-        const movieId = movie.id;
-        console.log(movieId);
-        
-        fetch(`http://localhost:8000/api/v1/titles/`+ movieId)
-            .then(res => res.json())
-            .then(movieDetails => {
-                console.log("pouet");
-                // element image
-                const best_film_img = document.getElementById("image_url");
-                best_film_img.src = movieDetails.image_url;
-                best_film_img.alt = movieDetails.title;
-                // element titre
-                const best_film_title = document.getElementById("title");
-                console.log(best_film_title);
-                best_film_title.textContent = movieDetails.title;
-                // element description
-                const best_film_description = document.getElementById("description");
-                console.log(best_film_description);
-                best_film_description.innerText = movieDetails.description;
+function displayBestMovie() {
+    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=1&page=1')
+        .then(res => res.json())
+        .then(data => {
+
+            fetch(`http://localhost:8000/api/v1/titles/`+ data.results[0].id)
+                .then(res => res.json())
+                .then(movieDetails => {
+                    // element image
+                    const best_film_img = document.getElementById("best_film_img");
+                    best_film_img.src = movieDetails.image_url;
+                    best_film_img.alt = movieDetails.title;
+                    // element titre
+                    const best_film_title = document.getElementById("best_film_title");
+                    best_film_title.innerText = movieDetails.title;
+                    // element description
+                    const best_film_description = document.getElementById("best_film_description");
+                    best_film_description.innerText = movieDetails.description;
+
+                    const btnDetail = document.getElementById("modal_open_btn")
+                    btnDetail.dataset.id = movieDetails.id
+                })
+            .catch(err => console.log(err));
             })
         .catch(err => console.log(err));
+}
+
+function fetchDataCategory(catUrl){
+    fetch(`http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=${catUrl}&page_size=6`) 
+        .then(res => res.json())
+        .then(movies => {
+            displayCategory(movies.results, catUrl)
         })
-    .catch(err => console.log(err));
+        .catch(err => console.log(err));
+}
 
-/***** Information catégorie Animation *****/
-//http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Animation
+function displayCategory(catResult, catText){
+    const catCategory = document.querySelector(".category")
 
-/***** Information catégorie Sci-Fi 15 "name": "Sci-Fi" *****/
-//http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Sci-Fi
+    const catTitle = document.createElement("h2");
+    catTitle.classList.add("category_title_section");
+    catTitle.textContent = catText;
 
-/***** Information catégorie Action 14  *****/
-// http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Action
+    const catContainer = document.createElement("div");
+    catContainer.classList.add("category_film_container");
 
-// /****** Adapté le nombre de film a la taille de l'ecran *****/
-// function getNumberOfFilms() {
-//     const screenWidth = window.innerWidth;
-//     let numberOfFilms;
+    catCategory.appendChild(catTitle);
+    catCategory.appendChild(catContainer);
+    console.log(catResult.length);
 
-//     // Définir le nombre de films en fonction de la taille de l'écran
-//     if (screenWidth >= 1054) {
-//         numberOfFilms = 6;
-//     } else if (screenWidth >= 788) {
-//         numberOfFilms = 4; 
-//     } else {
-//         numberOfFilms = 2; 
-//     }
-//     return numberOfFilms;
-// }
+    catResult.forEach((film, index) => {
 
+        const catFilm = document.createElement("div");
+        catFilm.classList.add("category_film");
 
+        const catImg = document.createElement("img");
+        catImg.classList.add("category_film_img")
+        catImg.src = film.image_url;
+        catImg.alt = film.title;
 
+        const catContent = document.createElement("div");
+        catContent.classList.add("category_film_content");
+
+        const catSubTitle = document.createElement("h3");
+        catSubTitle.classList.add("category_film_title");
+        catSubTitle.innerText = film.title;
+
+        const catBtn = document.createElement("button");
+        catBtn.classList.add("category_film_button");
+        catBtn.id = `catBtnId-${film.id}`
+        catBtn.dataset.id = film.id
+        catBtn.textContent = "Détails"
+
+        catContent.appendChild(catSubTitle);
+        catContent.appendChild(catBtn);
+
+        catFilm.appendChild(catImg);
+        catFilm.appendChild(catContent);
+
+        catContainer.appendChild(catFilm);
+
+        catBtn.addEventListener("click", (e) => {
+            const movieId = e.target.dataset.id
+            displayModal(movieId);
+        })
+
+     } )
+}
--- js/modal.js
+++ js/modal.js
@@ -5,63 +5,64 @@ const showButton = document.getElementById("modal_open_btn");
 const closeButton = document.getElementById("modal_close_btn");
 
 // Le bouton "Afficher la fenêtre" ouvre le dialogue
-showButton.addEventListener("click", () => {
-    console.log("pouet");
-    dialog.showModal();
+showButton.addEventListener("click", (e) => {
+    const movieId = e.target.dataset.id
+    console.log(movieId);
+    displayModal(movieId);
+    
 });
-  
+
 // Le bouton "Fermer" ferme le dialogue
 closeButton.addEventListener("click", () => {
     dialog.close();
 });
 
 /***** Information sur le film *****/
-fetch('http://localhost:8000/api/v1/titles/')
-    .then(res => res.json())
-    .then(movie => {
-        const movieId = movie.id;
+function displayModal(movieId){
+    console.log("prout");
+            fetch(`http://localhost:8000/api/v1/titles/`+ movieId)
+                .then(res => res.json())
+                .then(movieDetails => {
 
-        fetch(`http://localhost:8000/api/v1/titles/`+ movieId)
-            .then(res => res.json())
-            .then(movieDetails => {
-                console.log(movieDetails);
-                // element image
-                const modal_img = document.getElementById("image_url");
-                modal_img.src = movieDetails.image_url;
-                modal_img.alt = movieDetails.title;
-                // element titre
-                const modal_detail_title = document.getElementById("title");
-                modal_detail_title.textContent = movieDetails.title;
-                // element description
-                const modal_description_film = document.getElementById("long_description");
-                modal_description_film.textContent = movieDetails.long_description;
-                // element année
-                const modal_detail_date = document.getElementById("year");
-                modal_detail_date.textContent = movieDetails.year;
-                // element genre
-                const modal_detail_genre = document.getElementById("genres");
-                modal_detail_genre.textContent = movieDetails.genres;
-                // element durée du film
-                const modal_detail_during = document.getElementById("duration");
-                modal_detail_during.textContent = movieDetails.duration;
-                //element pays
-                const modal_detail_country = document.getElementById("countries");
-                modal_detail_country.textContent = movieDetails.countries;
-                // element IMDB
-                const modal_detail_IMDB = document.getElementById("imdb_score");
-                modal_detail_IMDB.textContent = movieDetails.imdb_score;
-                // element director
-                const modal_detail_director = document.getElementById("directors");
-                modal_detail_director.textContent = movieDetails.directors;
-                // element acteur
-                const modal_actor = document.getElementById("actors");
-                modal_actor.textContent = movieDetails.actors;
+                    // element image
+                    const modal_img = document.getElementById("modal_img");
+                    modal_img.src = movieDetails.image_url;
+                    modal_img.alt = movieDetails.title;
+                    // element titre
+                    const modal_detail_title = document.getElementById("modal_detail_title");
+                    modal_detail_title.textContent = movieDetails.title;
+                    // element description
+                    const modal_description_film = document.getElementById("long_description");
+                    modal_description_film.textContent = movieDetails.long_description;
+                    // element année
+                    const modal_detail_date = document.getElementById("modal_detail_date");
+                    modal_detail_date.textContent = movieDetails.year + " - ";
+                    // element genre
+                    const modal_detail_genre = document.getElementById("modal_detail_genre");
+                    modal_detail_genre.textContent = movieDetails.genres;
+                    // element durée du film
+                    const modal_detail_during = document.getElementById("modal_detail_duration");
+                    modal_detail_during.textContent = movieDetails.duration + " minutes";
+                    //element pays
+                    const modal_detail_country = document.getElementById("modal_detail_country");
+                    modal_detail_country.textContent = movieDetails.countries;
+                    // element IMDB
+                    const modal_detail_IMDB = document.getElementById("modal_detail_IMDB");
+                    modal_detail_IMDB.textContent = "IMDB score : " + movieDetails.imdb_score;
+                    // element director
+                    const modal_detail_director = document.getElementById("modal_detail_director");
+                    modal_detail_director.textContent = movieDetails.directors;
+                    // element acteur
+                    const modal_actor = document.getElementById("modal_actor_list");
+                    modal_actor.textContent = movieDetails.actors;
 
-                // element classification ????
-                const modal_detail_classification = document.getElementById("classification");
-                modal_detail_classification.textContent = movieDetails.classification;
+                    // element classification ????
+                    const modal_detail_classification = document.getElementById("modal_detail_classification");
+                    modal_detail_classification.textContent = movieDetails.classification;
 
-            })
-        .catch(err => console.log(err));
-        })
-    .catch(err => console.log(err));
+                    dialog.showModal();
+                })
+
+
+            .catch(err => console.log(err));
+}
